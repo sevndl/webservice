44f9ca79c9937643714f0638f60b98b905ffdc65
sevndl <nandillons@gmail.com>
2021-02-25 17:03:14 +0100

    première gestion de route

--- firstapp/server/main.js
+++ firstapp/server/main.js
@@ -1,5 +1,13 @@
 import { Meteor } from 'meteor/meteor';
+import { WebApp } from 'meteor/webapp'
 
 Meteor.startup(() => {
-  console.log("salut les nazes")
+  console.log("salut les nazes");
+});
+
+WebApp.connectHandlers.use('/api/test', (req, res, next) => {
+  // pour vérifier qu'on passe bien par ici
+  console.log('endpoint /api/test');
+  res.writeHead(200);
+  res.end("response from server");
 });
